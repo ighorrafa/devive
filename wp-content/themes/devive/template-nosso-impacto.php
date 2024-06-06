963b49292ae338c75f4a955d01350101faff5b6b
Firstname Lastname <matheus.vex@gmail.com>
2024-06-06 14:00:23 -0300

    feat: nosso impacto page

--- wp-content/themes/devive/template-nosso-impacto.php
+++ wp-content/themes/devive/template-nosso-impacto.php
@@ -2,6 +2,47 @@
 // Template Name: Nosso Impacto
 get_header(); ?>
 <main id="nosso-impacto">
-    <!-- CODAR AQUI -->
+    <section class="section-img-left">
+        <h1 class="color-dark-blue mb-5"><?php the_field('titulo_weimpact'); ?></h1>
+        <div class="d-flex justify-content-between">
+            <div class="image-wrapper col-lg-6">
+                <?php $imagemSecao1 = get_field('imagem_secao_1'); ?>
+                <?php echo wp_get_attachment_image($imagemSecao1['id'], 'full') ?>
+            </div>
+            <div class="col-lg-6 p-5 align-content-center">
+                <h2 class="color-dark-blue mb-5"><?php the_field('titulo_secao_1'); ?></h2>
+                <p><?php the_field('descricao_secao_1'); ?></p>
+            </div>
+        </div>
+    </section>
+    <section class="section-img-right bg-blue">
+        <div class="d-flex justify-content-between">
+            <div class="col-lg-6 align-content-center">
+                <h2 class="color-dark-blue mb-5 "><?php the_field('titulo_secao_2'); ?></h2>
+                <p><?php the_field('descricao_secao_2'); ?></p>
+            </div>
+            <div class="col-lg-6 p-5 image-wrapper">
+                <?php $imagemSecao1 = get_field('imagem_secao_2'); ?>
+                <?php echo wp_get_attachment_image($imagemSecao1['id'], 'full') ?>
+            </div>
+        </div>
+    </section>
+    <section class="section-img-left bg-yellow">
+        <div class="d-flex justify-content-between">
+            <div class="image-wrapper col-lg-6">
+                <?php $imagemSecao1 = get_field('imagem_secao_3'); ?>
+                <?php echo wp_get_attachment_image($imagemSecao1['id'], 'full') ?>
+            </div>
+            <div class="col-lg-6 p-5 align-content-center">
+                <h2 class="color-dark-blue mb-5"><?php the_field('titulo_secao_3'); ?></h2>
+                <p><?php the_field('descricao_secao_3'); ?></p>
+            </div>
+        </div>
+    </section>
+    <section class="transparency-and-eficience">
+        <div class="d-flex justify-content-center">
+            <h2 class="color-dark-blue mb-5"><?php the_field('titulo_secao_4'); ?></h2>
+        </div>
+    </section>
 </main>
 <?php get_footer(); ?>
