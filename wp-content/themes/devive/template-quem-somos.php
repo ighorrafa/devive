09362b6461fd33b5491f3528e1c35b0191753216
Firstname Lastname <matheus.vex@gmail.com>
2024-06-10 19:02:39 -0300

    feat: pagina quem somos final

--- wp-content/themes/devive/template-quem-somos.php
+++ wp-content/themes/devive/template-quem-somos.php
@@ -21,7 +21,7 @@ get_header(); ?>
                 <?php $imagemSecao1 = get_field('imagem_secao_2'); ?>
                 <?php echo wp_get_attachment_image($imagemSecao1['id'], 'full') ?>
            </div>
-           <div class="col-lg-6 p-5">
+           <div class="col-lg-6 p-5 d-flex flex-column justify-content-center">
                <h2 class="color-dark-blue"><?php the_field('titulo_secao_2'); ?></h2>
                <p class="color-dark-blue"><?php the_field('descricao_secao_2'); ?></p>
            </div>
@@ -31,35 +31,63 @@ get_header(); ?>
        <div class="d-flex justify-content-center">
             <h2 class="color-dark-blue"><?php the_field('titulo_secao_3'); ?></h2>
        </div>
-       <div>
+       <div class="row ">
            <?php
                 $itens = get_field('repetidor_secao_3');
                 foreach ($itens as $item) :
             ?>
-                <div>
-                    <div>
+                <div class="col-lg-6 px-5 d-flex flex-column justify-content-end">
+                    <div class="d-flex justify-content-center">
                         <?php $icon = $item['icone']; ?>
                         <?php echo wp_get_attachment_image($icon['id'], 'full') ?>
                     </div>
-                    <div>
-                        <h3 class="color-dark-blue m-0"><?php echo $item['titulo'] ?></h3>
-                        <p class="color-dark-blue m-0"><?php echo $item['descricao'] ?></p>
+                    <div class="d-flex justify-content-center">
+                        <div class="content-wrapper">
+                            <h3 class="color-dark-blue"><?php echo $item['titulo'] ?></h3>
+                            <p class="color-dark-blue"><?php echo $item['descricao'] ?></p>
+                        </div>
                     </div>
                 </div>
             <?php
                 endforeach;
             ?>
        </div>
+       <hr class="line">
+       <div class="d-flex justify-content-center">
+            <?php $imagem = get_field('icone_secao_3'); ?>
+            <?php echo wp_get_attachment_image($imagem['id'], 'full') ?>
+        </div>
+        <div class="content-wrapper-margin">
+            <h3 class="color-dark-blue"><?php the_field('subtitulo_secao_3'); ?></h3>
+        </div>
         <div class="d-flex flex-row justify-content-around">
             <?php
+                $count = 0;
                 $itens = get_field('imagens_secao_3');
                 foreach ($itens as $item) :
             ?>
-                <div>
+                <div data-bs-toggle="modal" data-bs-target="#modal<?php echo $count ?>">
                     <?php $icon = $item['imagem']; ?>
                     <?php echo wp_get_attachment_image($icon['id'], 'full') ?>
                 </div>
+                <div class="modal fade" id="modal<?php echo $count ?>" tabindex="-1" aria-labelledby="modal<?php echo $count ?>" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel"><?php echo $item['nome'] ?></h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                    <?php echo wp_get_attachment_image(142, 'full') ?>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p><?php echo $item['cargo'] ?></p>
+                                <span><?php echo $item['descricao'] ?></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             <?php
+                $count++;
                 endforeach;
             ?>
         </div>
@@ -73,11 +101,27 @@ get_header(); ?>
                 $itens = get_field('imagens_secao_4');
                 foreach ($itens as $item) :
             ?>
-                <div class="m-5">
+                <div class="m-5" data-bs-toggle="modal" data-bs-target="#modal<?php echo $count ?>">
                     <?php $icon = $item['imagem']; ?>
                     <?php echo wp_get_attachment_image($icon['id'], 'full') ?>
                 </div>
+                <div class="modal fade" id="modal<?php echo $count ?>" tabindex="-1" aria-labelledby="modal<?php echo $count ?>" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title mb-5" id="exampleModalLabel"><?php echo $item['nome'] ?></h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                    <?php echo wp_get_attachment_image(142, 'full') ?>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <span><?php echo $item['descricao'] ?></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             <?php
+                $count++;
                 endforeach;
             ?>
         </div>
