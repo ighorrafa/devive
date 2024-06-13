ce8210d86eb072ee3b5f3cff89cd509d54b9ba56
Firstname Lastname <matheus.vex@gmail.com>
2024-06-13 11:44:42 -0300

    feat: mobile resolution home page

--- wp-content/themes/devive/front-page.php
+++ wp-content/themes/devive/front-page.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 <main id="front-page">
     <section class="banner bg-yellow">
-        <div class="d-flex align-items-center justify-content-between">
-            <div class="content-wrapper">
-                <h1 class="color-dark-blue mb-5"><?php the_field('titulo'); ?></h1>
+        <div class="d-lg-flex align-items-center justify-content-between">
+            <div class="content-wrapper my-lg-0 my-5">
+                <h1 class="color-dark-blue mb-5 w-100"><?php the_field('titulo'); ?></h1>
                 <a class="btn bg-blue" href="<?php the_field('link') ?>"><?php the_field('label') ?></a>
             </div>
-            <div class="d-flex justify-content-end">
+            <div class="d-lg-flex justify-content-end">
                 <?php $banner = get_field('imagem'); ?>
                 <?php echo wp_get_attachment_image($banner['id'], 'full') ?>
             </div>
@@ -19,21 +19,21 @@
                     <h1 class="color-dark-blue mb-5"><?php the_field('titulo_dcnt'); ?></h1>
                     <p><?php the_field('descricao_dcnt'); ?></p>
                     <div class="mb-5"></div>
-                    <a class="btn bg-blue" href="<?php the_field('link') ?>"><?php the_field('label') ?></a>
+                    <a class="btn bg-blue" href="<?php the_field('link_dcnt') ?>"><?php the_field('label_dcnt') ?></a>
                 </div>
             </div>
         </div>
     </section>
     <section class="box">
         <div class="container bg-dark-blue box-wrapper w-75">
-            <div class="d-flex justify-content-around">
+            <div class="d-lg-flex justify-content-around">
                 <?php
                     $itens = get_field('itens');
                     foreach($itens as $item):
                 ?>
-                <div>
+                <div class="my-5 my-lg-0">
                     <div class="row justify-content-center">
-                        <h2 class="mb-2"><?php echo $item['titulo'] ?></h2>
+                        <h2 class="mb-2 d-flex"><?php echo $item['titulo'] ?></h2>
                         <p><?php echo $item['descricao'] ?></p>
                     </div>
                 </div>
