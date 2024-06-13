6d606b0621e39a62bc5853b4c5b5eafd7516b716
ighorrafa <rafaelighor@gmail.com>
2024-06-12 23:56:30 -0300

    interação modais

--- wp-content/themes/devive/template-quem-somos.php
+++ wp-content/themes/devive/template-quem-somos.php
@@ -3,38 +3,38 @@
 get_header(); ?>
 <main id="quem-somos">
     <section class="bg-blue secao1">
-       <div class="d-lg-flex">
-           <div class="col-lg-6">
-               <h2 class="color-dark-blue"><?php the_field('titulo_secao_1'); ?></h2>
-               <p class="color-dark-blue"><?php the_field('descricao_secao_1'); ?></p>
-               <a href="#"><?php the_field('link_secao_1'); ?></a>
-           </div>
-           <div class="col-lg-6 my-lg-0 my-5">
+        <div class="d-lg-flex">
+            <div class="col-lg-6">
+                <h2 class="color-dark-blue"><?php the_field('titulo_secao_1'); ?></h2>
+                <p class="color-dark-blue"><?php the_field('descricao_secao_1'); ?></p>
+                <a href="#"><?php the_field('link_secao_1'); ?></a>
+            </div>
+            <div class="col-lg-6 my-lg-0 my-5">
                 <?php $imagemSecao1 = get_field('imagem_secao_1'); ?>
                 <?php echo wp_get_attachment_image($imagemSecao1['id'], 'full') ?>
-           </div>
-       </div>
+            </div>
+        </div>
     </section>
     <section class="secao2">
-       <div class="d-lg-flex flex-row-reverse">
+        <div class="d-lg-flex flex-row-reverse">
             <div class="col-lg-6 p-5 d-flex flex-column justify-content-center">
-               <h2 class="color-dark-blue"><?php the_field('titulo_secao_2'); ?></h2>
-               <p class="color-dark-blue"><?php the_field('descricao_secao_2'); ?></p>
-           </div>
-           <div class="col-lg-6">
+                <h2 class="color-dark-blue"><?php the_field('titulo_secao_2'); ?></h2>
+                <p class="color-dark-blue"><?php the_field('descricao_secao_2'); ?></p>
+            </div>
+            <div class="col-lg-6">
                 <?php $imagemSecao1 = get_field('imagem_secao_2'); ?>
                 <?php echo wp_get_attachment_image($imagemSecao1['id'], 'full') ?>
-           </div>
-       </div>
+            </div>
+        </div>
     </section>
     <section class="bg-yellow secao3 p-5">
-       <div class="d-flex justify-content-center">
+        <div class="d-flex justify-content-center">
             <h2 class="color-dark-blue"><?php the_field('titulo_secao_3'); ?></h2>
-       </div>
-       <div class="row">
-           <?php
-                $itens = get_field('repetidor_secao_3');
-                foreach ($itens as $item) :
+        </div>
+        <div class="row">
+            <?php
+            $itens = get_field('repetidor_secao_3');
+            foreach ($itens as $item) :
             ?>
                 <div class="col-lg-6 px-5 d-flex flex-column justify-content-end">
                     <div class="d-flex justify-content-center">
@@ -49,11 +49,11 @@ get_header(); ?>
                     </div>
                 </div>
             <?php
-                endforeach;
+            endforeach;
             ?>
-       </div>
-       <hr class="line">
-       <div class="d-flex justify-content-center">
+        </div>
+        <hr class="line">
+        <div class="d-flex justify-content-center">
             <?php $imagem = get_field('icone_secao_3'); ?>
             <?php echo wp_get_attachment_image($imagem['id'], 'full') ?>
         </div>
@@ -62,13 +62,21 @@ get_header(); ?>
         </div>
         <div class="d-flex flex-row justify-content-around">
             <?php
-                $count = 0;
-                $itens = get_field('imagens_secao_3');
-                foreach ($itens as $item) :
+            $count = 0;
+            $itens = get_field('imagens_secao_3');
+            foreach ($itens as $item) :
             ?>
-                <div data-bs-toggle="modal" data-bs-target="#modal<?php echo $count ?>">
-                    <?php $icon = $item['imagem']; ?>
-                    <?php echo wp_get_attachment_image($icon['id'], 'full') ?>
+                <div class="modal-card position-relative" data-bs-toggle="modal" data-bs-target="#modal<?php echo $count ?>">
+                    <figure class="h-100 w-100 overflow-hidden">
+                        <?php $icon = $item['imagem']; ?>
+                        <?php echo wp_get_attachment_image($icon['id'], 'full') ?>
+                    </figure>
+                    <div class="card-wrapper d-flex flex-column justify-content-between">
+                        <h5><?php echo $item['nome']; ?></h5>
+                        <div>
+                            <div class="btn-card">Clique para saber mais</div>
+                        </div>
+                    </div>
                 </div>
                 <div class="modal fade" id="modal<?php echo $count ?>" tabindex="-1" aria-labelledby="modal<?php echo $count ?>" aria-hidden="true">
                     <div class="modal-dialog">
@@ -88,22 +96,30 @@ get_header(); ?>
                 </div>
             <?php
                 $count++;
-                endforeach;
+            endforeach;
             ?>
         </div>
     </section>
     <section class="secao4">
-       <div class="d-flex justify-content-center">
+        <div class="d-flex justify-content-center">
             <h2 class="color-dark-blue"><?php the_field('titulo_secao_4'); ?></h2>
-       </div>
-       <div class="d-flex flex-row justify-content-around">
+        </div>
+        <div class="d-flex flex-row justify-content-around">
             <?php
-                $itens = get_field('imagens_secao_4');
-                foreach ($itens as $item) :
+            $itens = get_field('imagens_secao_4');
+            foreach ($itens as $item) :
             ?>
-                <div class="m-5" data-bs-toggle="modal" data-bs-target="#modal<?php echo $count ?>">
-                    <?php $icon = $item['imagem']; ?>
-                    <?php echo wp_get_attachment_image($icon['id'], 'full') ?>
+                <div class="modal-card position-relative m-5" data-bs-toggle="modal" data-bs-target="#modal<?php echo $count ?>">
+                    <figure class="h-100 w-100 overflow-hidden">
+                        <?php $icon = $item['imagem']; ?>
+                        <?php echo wp_get_attachment_image($icon['id'], 'full') ?>
+                    </figure>
+                    <div class="card-wrapper d-flex flex-column justify-content-between">
+                        <h5><?php echo $item['nome']; ?></h5>
+                        <div>
+                            <div class="btn-card">Clique para saber mais</div>
+                        </div>
+                    </div>
                 </div>
                 <div class="modal fade" id="modal<?php echo $count ?>" tabindex="-1" aria-labelledby="modal<?php echo $count ?>" aria-hidden="true">
                     <div class="modal-dialog">
@@ -122,7 +138,7 @@ get_header(); ?>
                 </div>
             <?php
                 $count++;
-                endforeach;
+            endforeach;
             ?>
         </div>
     </section>
